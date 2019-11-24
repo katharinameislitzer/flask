01f5f8326f37488df04e594efb812b1e8b5419c6
katharinameislitzer <55828429+katharinameislitzer@users.noreply.github.com>
2019-11-24 11:28:23 +0100

    flask neu

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 import flask
 import random
 import model
+
+from flask import url_for
+
 app = flask.Flask(__name__)
+# db = model.db
+# db.create_all()
 
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html", myname="Kathi") #myname platzhalter-variable
+   # user = model.User(id=1, username="Wolfi", email="wolfi@haus.at")
+  #  db.add(user)
+    # db.commit()
+
+    return flask.render_template("index.html", myname="Kathi")  # myname platzhalter-variable
+
 
 @app.route("/fakebook")
 def fakebook():
     return flask.render_template("fakebook.html")
 
+
 @app.route("/portfolio")
 def portfolio():
     return flask.render_template("portfolio.html")
+
 
 @app.route("/about")
 def about():
     return flask.render_template("about.html")
 
-@app.route("/secret-number-game")
+
+@app.route("/secret_number_game")
 def secret_number_game():
-    return flask.render_template("secret_number_game.html", secret_number=random.randint(1,10))
+    return flask.render_template("secret_number_game.html", secret_number=random.randint(1, 10))
+
 
 @app.route("/blog")
 def blog():
-
     receipe_1 = model.Receipe("Apfelstrudel", "Cut Apple Bake Sweet", "sweet")
     receipe_2 = model.Receipe("Hamburger", "Fry Meat And Eat", "savioury")
     receipe_3 = model.Receipe("Suppe", "Cut Carrots Add Water", "salty")
@@ -34,7 +47,17 @@ def blog():
     return flask.render_template("blog.html", receipes=[receipe_1, receipe_2, receipe_3])
 
 
+@app.route("/books")
+def books():
+    book_1 = model.Book("The idiot", "Dostojewski", "Lorem ipsum")
+    book_2 = model.Book("It", "King", "Lorem ipsumsed diam nonumy eirmod tempor invidunt")
+    book_3 = model.Book("Wuthering Heights", "Bronte", "Lorem ipsum dolor sit amet, conse")
 
+    return flask.render_template("books.html", books=[book_1, book_2, book_3])
+
+@app.route("/katzensalon")
+def katzensalon():
+    return flask.render_template("katzensalon.html")
 
 if __name__ == '__main__':
     app.run()
--- model.py
+++ model.py
@@ -1,5 +1,22 @@
+# import sqla_wrapper
+import os
+
+
+# db = sqla_wrapper.SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
+
+# class User(db.Model):
+   # id = db.Column(db.Integer, primary_key=True)
+   # username = db.Column(db.String, unique=True)
+    # email = db.Column(db.String, unique=True)
+
 class Receipe:
     def __init__(self, name, description, taste):
         self.name = name
         self.description = description
         self.taste = taste
+
+class Book:
+    def __init__(self, title, author, description):
+        self.title = title
+        self.author = author
+        self.description = description
