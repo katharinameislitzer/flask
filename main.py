a96128763942c51d5e30ceb99f5e2f37d415e4b5
katharinameislitzer <55828429+katharinameislitzer@users.noreply.github.com>
2019-12-16 19:39:42 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import model
 import string
 import hashlib
 import uuid
+import requests
+
+OPENWEATHERMAP_API_KEY = "55d79b326f3720654f6ab31b68231d4a"
 
 from flask import request
 from flask import url_for
@@ -18,6 +21,47 @@ app = flask.Flask(__name__)
 db = model.db
 
 db.create_all()
+
+
+class WeatherInfo:
+    def __init__(self,city_name, country, temp, wind_speed, wind_deg):
+        self.city_name = city_name
+        self.country = country
+        self.temp_K = temp
+        if self.temp_K is not None:
+            self.temp_C = round(self.temp_K - 273, 2)
+        else:
+            self.temp_C = None
+        self.wind_speed = wind_speed
+        self.wind_deg = wind_deg
+
+    @staticmethod
+    def from_json(json_data):
+            city_name = json_data['name']
+            country_name = json_data ['sys']['country']
+            temp = json_data['main']['temp']
+            wind_speed = json_data['wind'].get('speed', 0)
+            wind_deg = json_data['wind'].get('deg', 0)
+            weather_info = WeatherInfo(
+                city_name, country_name, temp, wind_speed, wind_deg
+            )
+            return weather_info
+
+def get_weather_data(city_name):
+    site_url = "api.openweathermap.org/data/2.5/weather"
+    city = f"q={city_name}"
+    app_id = f"appid={OPENWEATHERMAP_API_KEY}"
+
+
+    url=f"https://{site_url}?{city}&{app_id}"
+    response = requests.get(url)
+    json_data = response.json()
+    if json_data['cod'] !=200:
+        weather_info = WeatherInfo(city_name, None, None, None, None)
+    else:
+        weather_info = WeatherInfo.from_json(json_data)
+    return weather_info
+
 
 hasher = hashlib.blake2s()
 
@@ -387,6 +431,12 @@ def logout():
     db.commit()
 
     return flask.redirect(flask.url_for('login'))
+
+@app.route("/weather/<city_name>", methods=["GET"])
+def weather(city_name):
+    weather_info = get_weather_data(city_name)
+    return flask.render_template('weather.html', weather_info=weather_info)
+
 
 
 if __name__ == '__main__':
